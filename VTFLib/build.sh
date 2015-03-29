44491dc69d3dc185b7a6430b39ad8f5087c64bee
Robin Appelman <icewind@owncloud.com>
2015-03-29 11:48:42 +0200

    optimize builds

--- VTFLib/build.sh
+++ VTFLib/build.sh
@@ -1,10 +1,25 @@
 #! /usr/bin/bash
 
 browserify -o lib-bundle.js lib.js
-emcc -o ../src/vtflib.js  --js-library lib-bundle.js squish.bc ./*.cpp -s EXPORTED_FUNCTIONS="['_fromData','_vlImageGetSize','_vlImageSaveLump']"
+emcc -O2 -o ../src/vtflib.js  --memory-init-file 0  --js-library lib-bundle.js squish.bc ./*.cpp -s EXPORTED_FUNCTIONS="['_fromData','_vlImageGetSize','_vlImageSaveLump']"
 
-#// The environment setup code below is customized to use Module.
-#// *** Environment setup code ***
+# make the result work with browserify
+
+MATCH='var ENVIRONMENT_IS_NODE=typeof process==="object"&&typeof require==="function";var ENVIRONMENT_IS_WEB=typeof window==="object";var ENVIRONMENT_IS_WORKER=typeof importScripts==="function";'
+
+REPLACE='ENVIRONMENT_IS_WEB=typeof window==="object";var ENVIRONMENT_IS_WORKER=typeof importScripts==="function";var ENVIRONMENT_IS_NODE=typeof process==="object"&&typeof require==="function"&&!ENVIRONMENT_IS_WEB&&!ENVIRONMENT_IS_WORKER;if(typeof module!=="undefined"){module["exports"] = Module;}'
+
+MATCH=$(sed -e 's/[]\/$*.^|[]/\\&/g' <<< $MATCH)
+REPLACE=$(sed -e 's/[\/&]/\\&/g' <<< $REPLACE)
+
+sed -i "s/$MATCH/$REPLACE/" ../src/vtflib.js
+
+# we're not using websockets and browserify complains about it
+sed -i 's/require..ws../null/' ../src/vtflib.js
+
+
+# The header we want
+
 #var ENVIRONMENT_IS_WEB = typeof window === 'object';
 #var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
 #var ENVIRONMENT_IS_NODE = (typeof process === 'object' && typeof require === 'function') && !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_WORKER;
